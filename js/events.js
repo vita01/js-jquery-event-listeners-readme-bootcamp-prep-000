a3a44edc757e0d0087e6769a84280f4b740ff521
Victoria <vitalina27@gmail.com>
2017-11-08 14:20:05 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -6,6 +6,29 @@ function getIt(){
     alert("Hey!");
   });
 }
+function frameIt(){
+  $(document).ready(function(){
+$('img').addClass('tasty');
+
+  });
+}
+
+function pressIt(){
+
+  $('typing').keydown(function(e){
+if(e.which==71){
+  alert('Wow');
+}
+
+  });
+}
+function submitIt(){
+$('typing').submit(function(e){
+alert("Your form is going to be submitted now.");
+
+});
+
+}
 $(document).ready(function(){
 
 // call functions here
