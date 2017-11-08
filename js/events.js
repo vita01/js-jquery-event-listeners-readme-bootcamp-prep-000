41ad37ee674784832a4dacf4b89096e4f695e672
Victoria <vitalina27@gmail.com>
2017-11-08 14:23:57 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,65 @@
 //define functions here
+function getIt(){
 
+  $('p').on('click',function(){
+
+    alert("Hey!");
+  });
+}
+function frameIt(){
+  $(document).ready(function(){
+$('img').addClass('tasty');
+
+  });
+}
+
+function pressIt(){
+
+  $('*').keydown(function(e){
+if(e.which==71){
+  alert('Wow');
+}
+
+  });
+}
+function submitIt(){
+$('form').submit(function(e){
+alert("Your form is going to be submitted now.");
+
+});
+
+}
 $(document).ready(function(){
 
 // call functions here
+function getIt(){
 
+  $('p').on('click',function(){
+
+    alert("Hey!");
+  });
+}
+function frameIt(){
+  $(document).ready(function(){
+$('img').addClass('tasty');
+
+  });
+}
+
+function pressIt(){
+
+  $('typing').keydown(function(e){
+if(e.which==71){
+  alert('Wow');
+}
+
+  });
+}
+function submitIt(){
+$('typing').submit(function(e){
+alert("Your form is going to be submitted now.");
+
+});
+
+}
 });
